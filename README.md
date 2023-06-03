06f1366e30a2cc3535548eff8b07baa457a1314b
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-03 20:39:54 +0700

    `component&props` add 3 button

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 style="text-align: center">React 18</h1>
+
+This project is a simple example of how to use React 18 with the new documentation from [react](https://react.dev/) and this project learn from [Avip Code Studio](https://www.youtube.com/@vipcodestudio).
+
+Documentations that used in this project :
+1. [Old Documentation React](https://legacy.reactjs.org/)
+2. [New Documentation React](https://react.dev/)
+3. [Vite](https://vitejs.dev/)
+4. [Tailwind CSS](https://tailwindcss.com/)
+
+## Components and Props
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-3xl font-bold underline">Hello World!</div>;
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-2">
+        <button className="h-10 px-6 font-semibold rounded-md bg-black hover:bg-slate-900 text-white">Buy Now</button>
+        <button className="h-10 px-6 font-semibold rounded-md bg-black hover:bg-slate-900 text-white">Buy Now</button>
+        <button className="h-10 px-6 font-semibold rounded-md bg-black hover:bg-slate-900 text-white">Buy Now</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
