094823dd42803c9cd8e33dfc88bb5ad3ef741ee5
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-03 23:26:54 +0700

    `component&props` add props.variant for choose color

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import React from "react";
 import "./App.css";
 
-class ButtonClass extends React.Component {
-  render() {
-    return <button className="h-10 px-6 font-semibold rounded-md bg-black hover:bg-slate-900 text-white">Buy Now</button>;
-  }
-}
-
-const ButtonFunctional = () => {
-  return <button className="h-10 px-6 font-semibold rounded-md bg-black/60 hover:bg-slate-900/60 text-white">Buy Now</button>;
+const Button = (props) => {
+  return <button className={`h-10 px-6 font-semibold rounded-md hover:bg-slate-600 text-white ${props.variant}`}>Buy Now</button>;
 };
 
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-2">
-        <ButtonClass />
-        <ButtonClass />
-        <ButtonFunctional />
+        <Button variant="bg-slate-800" />
+        <Button variant="bg-red-800" />
       </div>
     </div>
   );
