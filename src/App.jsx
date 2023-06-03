278480a73d5ca74a4a961758bf975504d2e6e6e2
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-03 23:36:11 +0700

    `component&props` add props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 
 const Button = (props) => {
-  return <button className={`h-10 px-6 font-semibold rounded-md hover:bg-slate-600 text-white ${props.variant}`}>Buy Now</button>;
+  return <button className={`h-10 px-6 font-semibold rounded-md hover:bg-slate-600 text-white ${props.variant}`}>{props.children}</button>;
 };
 
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-2">
-        <Button variant="bg-slate-800" />
-        <Button variant="bg-red-800" />
+        <Button variant="bg-slate-800">Log In</Button>
+        <Button variant="bg-red-800">Log Out</Button>
+        <Button></Button>
       </div>
     </div>
   );
