75870c3ce79de1d662f1d803c8809bcf55bf2621
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-04 07:28:25 +0700

    `atomic design` create page login and layout auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import FormLogin from "./components/Fragments/FormLogin";
+import LoginPage from "./Pages/login";
 
 function App() {
   return (
     <div className="flex justify-center bg-white min-h-screen items-center">
-      <div className="w-full max-w-xs">
-        <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
-        <p className="font-medium text-slate-500 mb-8">Welcome, Please enter your details</p>
-        <FormLogin />
-      </div>
+      <LoginPage />
     </div>
   );
 }
