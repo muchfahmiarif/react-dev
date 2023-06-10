3cfc0c4613d0fb212b962de5362bbc06bb105c4a
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-10 12:26:16 +0700

    `event handler` add email and password to localStorage

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -4,6 +4,8 @@ import Button from "../Elements/Button";
 const FormLogin = () => {
   const handleLogin = (e) => {
     e.preventDefault(); // untuk mencegah reload
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("password", e.target.password.value);
     console.log(e.target.email.value);
     console.log(e.target.password.value);
     console.log("anda mengklik tombol ini");
