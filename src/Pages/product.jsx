e1fedf04051d08110bdd4120bf8962d4dc2964b2
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-10 08:52:23 +0700

    `rendering list` add 1 another product

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -3,12 +3,19 @@ import shoes from "../../public/fashion-shoes-sneakers.jpg";
 
 const product = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center gap-2">
       <CardProduct>
         <CardProduct.Header image={shoes} />
-        <CardProduct.Body title="Test">
+        <CardProduct.Body title="Sepatu Lama">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet
           voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.
+        </CardProduct.Body>
+        <CardProduct.Footer>Rp 1.000.000</CardProduct.Footer>
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header image={shoes} />
+        <CardProduct.Body title="Sepatu Baru">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error,
         </CardProduct.Body>
         <CardProduct.Footer>Rp 1.000.000</CardProduct.Footer>
       </CardProduct>
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   // eslint-disable-next-line react/prop-types
   const { children } = props;
-  return <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">{children}</div>;
+  return <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow flex flex-col justify-between">{children}</div>;
 };
 
 const Footer = (props) => {
@@ -31,7 +31,7 @@ const Body = (props) => {
   // eslint-disable-next-line react/prop-types
   const { children, title } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="#">
         <h5 className="text-white text-xl font-semibold tracking-tight">{title}</h5>
         <p className="text-sm text-white">{children}</p>
