28fb049a03d2b8569f014743bf9eead59e75c90e
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-10 12:12:23 +0700

    `event handler` add handler button

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -2,7 +2,10 @@ const Button = (props) => {
   // eslint-disable-next-line react/prop-types
   const { color, children, submit } = props;
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md hover:${color}/60 w-full text-white ${color}`} type={submit}>
+    <button
+      className={`h-10 px-6 font-semibold rounded-md hover:${color}/60 w-full text-white ${color}`}
+      type={submit}
+      onClick={() => console.log("anda mengklik tombol ini")}>
       {children}
     </button>
   );
