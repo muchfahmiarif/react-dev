7413cd15153afe68a83a54225e7cd366e2054580
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-11 20:50:47 +0700

    `getApiData` add function if product in null

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -5,45 +5,46 @@ import Button from "../components/Elements/Button";
 import getProducts from "../services/product.service";
 
 // eslint-disable-next-line react-refresh/only-export-components
-const products = [
-  {
-    id: 1,
-    title: "Sepatu Lama",
-    price: 1000000,
-    image: shoes,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
-  },
-  {
-    id: 2,
-    title: "Sepatu Baru",
-    price: 2000000,
-    image: shoes,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
-  },
-  {
-    id: 3,
-    title: "Sepatu Keren",
-    price: 3000000,
-    image: shoes,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     title: "Sepatu Lama",
+//     price: 1000000,
+//     image: shoes,
+//     description:
+//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
+//   },
+//   {
+//     id: 2,
+//     title: "Sepatu Baru",
+//     price: 2000000,
+//     image: shoes,
+//     description:
+//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
+//   },
+//   {
+//     id: 3,
+//     title: "Sepatu Keren",
+//     price: 3000000,
+//     image: shoes,
+//     description:
+//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus id voluptate cum culpa possimus veniam, error, velit repudiandae amet voluptates laborum ullam beatae quasi nesciunt suscipit delectus facere praesentium at.",
+//   },
+// ];
 
 const email = localStorage.getItem("email");
 
 const Product = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, []);
 
   useEffect(() => {
-    if (cart.length > 0) {
+    if (products.length > 0 && cart.length > 0) {
       const total = cart.reduce((prev, item) => {
         const product = products.find((product) => product.id === item.id);
         return prev + product.price * item.qty;
@@ -94,15 +95,16 @@ const Product = () => {
       </div>
       <div className="flex justify-center gap-2 mt-2">
         <div className="w-3/4 flex flex-wrap">
-          {products.map((item) => (
-            <CardProduct key={item.id}>
-              <CardProduct.Header image={item.image} />
-              <CardProduct.Body title={item.title}>{item.description}</CardProduct.Body>
-              <CardProduct.Footer id={item.id} handleAddToCart={handleAddToCart}>
-                {item.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}
-              </CardProduct.Footer>
-            </CardProduct>
-          ))}
+          {products.length > 0 &&
+            products.map((item) => (
+              <CardProduct key={item.id}>
+                <CardProduct.Header image={item.image} />
+                <CardProduct.Body title={item.title}>{item.description}</CardProduct.Body>
+                <CardProduct.Footer id={item.id} handleAddToCart={handleAddToCart}>
+                  {item.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}
+                </CardProduct.Footer>
+              </CardProduct>
+            ))}
         </div>
         <div className="w-1/4">
           <h1 className="text-3xl font-bold text-blue-600">Cart</h1>
@@ -121,17 +123,18 @@ const Product = () => {
               </tr>
             </thead>
             <tbody>
-              {cart.map((items) => {
-                const product = products.find((item) => item.id === items.id);
-                return (
-                  <tr key={product.id}>
-                    <td>{product.title}</td>
-                    <td>{items.qty}</td>
-                    <td>{product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
-                    <td>{(items.qty * product.price).toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
-                  </tr>
-                );
-              })}
+              {products.length > 0 &&
+                cart.map((items) => {
+                  const product = products.find((item) => item.id === items.id);
+                  return (
+                    <tr key={product.id}>
+                      <td>{product.title}</td>
+                      <td>{items.qty}</td>
+                      <td>{product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
+                      <td>{(items.qty * product.price).toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
+                    </tr>
+                  );
+                })}
               <tr ref={totalPriceRef}>
                 <td colSpan="3">
                   <b>Total</b>
