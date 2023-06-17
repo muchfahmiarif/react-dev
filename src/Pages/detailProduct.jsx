2f0b1d03cf5d457eebf623b3e6d9064c8a28b826
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-17 15:50:45 +0700

    `dynamin routing` get name product from api

--- src/Pages/detailProduct.jsx
+++ src/Pages/detailProduct.jsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDetailProduct } from "../services/product.service";
 
 const DetailProduct = () => {
   const { id } = useParams(); // useParams untuk mengambil parameter dari url
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
     getDetailProduct(id, (data) => {
-      console.log(data);
+      // console.log(data);
+      setProduct(data);
     });
   }, [id]);
-  return <div>detailProduct : {id}</div>;
+  console.log(product);
+  return <div>detailProduct : {product.title}</div>;
 };
 
 export default DetailProduct;
