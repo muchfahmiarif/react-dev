03481d967820148e1d8dbd60fd9658c80b562397
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-18 02:03:29 +0700

    `redux state management` add reducer

--- redux.js
+++ redux.js
@@ -1,5 +1,22 @@
 // reducer
-const cartReducer = (state, action) => {};
+const cartReducer = (
+  state = {
+    // state merupakan nilai awal
+    cart: [],
+  },
+  action
+) => {
+  // di dalam functin ini akan handle action yang dikirimkan, action merupakan object yang berisi type dan payload
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state, // mengambil state yang lama
+        cart: [...state.cart, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 // store
 
