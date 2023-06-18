08a90860318bb54e41f2e3dee0a9d26d11359ae4
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-18 15:57:52 +0700

    `implement readux` add slice cart

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions; // export function addToCart
+export default cartSlice.reducer; // export reducer
