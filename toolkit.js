69b518e8ca341a2244ed7c6ca22c84d93f7fa01e
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-18 13:24:20 +0700

    `redux toolkit` add createaction, input on reducer

--- toolkit.js
+++ toolkit.js
@@ -3,16 +3,18 @@ import { build } from "vite";
 
 const { configureStore, createAction, createReducer } = toolkit;
 
-// Reducer
+const addToCart = createAction("ADD_TO_CART"); // => action creator
 
+// Reducer
 const cartReducer = createReducer([], (builder) => {
   // array kosong merupakan cart reducer karena pada pilih reuducer di store, reducer yang dipilih adalah cart
-  builder.addCase("ADD_TO_CART", (state, action) => {
+  builder.addCase(addToCart, (state, action) => {
     state.push(action.payload); // => cara ke 1 ; pilih state mana yang mau diubah
     // ada 2 cara; 1. push, 2. spread operator
     // state.cart = [...state.cart, action.payload]; => cara ke 2
   });
 });
+// kalo ingin tambah reducer (login reducer) maka buat lagi seperti di atas
 
 // Store
 const store = configureStore({
@@ -25,3 +27,11 @@ const store = configureStore({
 // Subscribe
 
 // Dispatch
+const action1 = {
+  type: "ADD_TO_CART", // type harus sama dengan yang ada di reducer
+  payload: {
+    id: 2,
+    name: "Baju",
+  },
+};
+store.dispatch(action1);
