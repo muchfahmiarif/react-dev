86319137a2b2a20d01cbd2b501845cdedba487ba
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-24 10:18:39 +0700

    `implement redux` add total item in cart on navbar

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { useLogin } from "../../hooks/useLogin";
 import Button from "../Elements/Button";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const username = useLogin();
+  const [totalCart, setTotalCart] = useState(0);
+  const cart = useSelector((state) => {
+    return state.cart.data;
+  });
+
+  useEffect(() => {
+    const total = cart.reduce((prev, item) => {
+      return prev + item.qty;
+    }, 0);
+    setTotalCart(total);
+  }, [cart]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -15,6 +28,7 @@ const Navbar = () => {
       <Button color={`ml-5 bg-black`} onClick={handleLogout}>
         Logout
       </Button>
+      <div className="flex items-center bg-gray-400 p-2 rounded ml-5">{totalCart}</div>
     </div>
   );
 };
