351222797699198b1934bfcfde4182f34778df89
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-26 23:45:38 +0700

    `useReducer` add reducer total price

---
+++ src/context/TotalPriceContext.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { createContext, useReducer, useContext } from "react";
+
+const TotalPriceContext = createContext(null); // untuk menyimpan state
+const TotalPriceDispatchContext = createContext(null); // untuk menyimpan action
+
+const totalPriceReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_TOTAL_PRICE": {
+      return {
+        total: action.payload.total,
+      };
+    }
+    default: {
+      throw new Error(`Unknown action: ${action.type}`);
+    }
+  }
+};
+const TotalPriceProvider = ({ children }) => {
+  const [totalPrice, dispatch] = useReducer(totalPriceReducer, {
+    total: 0,
+  });
+
+  return (
+    <TotalPriceContext.Provider value={totalPrice}>
+      <TotalPriceDispatchContext.Provider value={dispatch}>{children}</TotalPriceDispatchContext.Provider>
+    </TotalPriceContext.Provider>
+  );
+};
+
+export default TotalPriceProvider;
